3edbed585ed99a6f444551a42f0b373fa94303e1
Matheus Alves <theuves@gmail.com>
2016-07-08 20:14:10 -0400

    build

--- dist/conjugador.js
+++ dist/conjugador.js
@@ -1,6 +1,9 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.conjugar = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+// Conjugador.js | Versão 0.0.2
+// Copyright (c) 2016 Matheus Alves
+
 var irregulares = require('./irregulares.json');
 
 module.exports = function (verbo) {
@@ -8,6 +11,10 @@ module.exports = function (verbo) {
 
     if (!/r$/.test(verbo)) {
         return new Error('Verbo inválido');
+    }
+
+    if (/[uo]r/.test(verbo)) {
+        return new Error('Não existem verbos terminados em -or ou -ur');
     }
 
     var conjugado = {};
